73b1899c6ee5863456bece671baca814813b41fe
heweiyan <heweiyan526@163.com>
2022-04-28 13:33:15 +0800

    把奇怪的空格符号改为正常的空格符号

--- app/src/test/java/com/yeah/learn/algorithm/medium/ContainerWithMostWaterUnitTest.java
+++ app/src/test/java/com/yeah/learn/algorithm/medium/ContainerWithMostWaterUnitTest.java
@@ -14,7 +14,7 @@ import org.junit.Test;
  * https://aliyun-lc-upload.oss-cn-hangzhou.aliyuncs.com/aliyun-lc-upload/uploads/2018/07/25/question_11.jpg
  * 输入：[1,8,6,2,5,4,8,3,7]
  * 输出：49
- * 解释：图中垂直线代表输入数组 [1,8,6,2,5,4,8,3,7]。在此情况下，容器能够容纳水（表示为蓝色部分）的最大值为 49。
+ * 解释：图中垂直线代表输入数组 [1,8,6,2,5,4,8,3,7]。在此情况下，容器能够容纳水（表示为蓝色部分）的最大值为 49。
  * <p>
  * 示例 2：
  * 输入：height = [1,1]
